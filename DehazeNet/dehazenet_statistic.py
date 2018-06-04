5ed4bbcf17ee7e2202562da2312ec3863cd2af9a
Seanforfun <32276204+Seanforfun@users.noreply.github.com>
2018-06-04 11:20:57 -0400

    [Function add]
    1.Add the of finding corresponding clear and result image.

--- DehazeNet/dehazenet_statistic.py
+++ DehazeNet/dehazenet_statistic.py
@@ -12,9 +12,12 @@ import threading
 
 GROUP_NUM = 10
 CHANNEL_NUM = 3
+COMMON_INDEX_BIT = 8
+CLEAR_DICTIONARY = {}
+DEPTH_DICTIONARY = {}
 # TODO Need to assign a directory
 clear_dir = ""
-hazy_dir = ""
+result_dir = ""
 depth_dir = ""
 
 DICT_WRITE_LOCK = threading.Lock()
@@ -31,15 +34,26 @@ def sta_cal_psnr(im1, im2, area, count):
     return psnr
 
 
-# TODO lz
-def sta_image_input(clear_dir, result_dir, depth_dir):
+# Read clear and depth images from dictionary and create two dictionary for the files for referencing
+def sta_image_input(clear_dir, depth_dir):
+    # First read the file names from clear directory
     clear_file_list = os.listdir(clear_dir)
-    result_file_list = os.listdir(result_dir)
     depth_file_list = os.listdir(depth_dir)
+    # Construct a dictionary for clear images
+    for clear_image_name in clear_file_list:
+        file_path = os.path.join(clear_dir, clear_image_name)
+        clear_index = clear_image_name[0:COMMON_INDEX_BIT]
+        CLEAR_DICTIONARY[clear_index] = file_path
 
-    return clear_file_list, result_file_list, depth_file_list
+    # Construct a dictionary for depth_images
+    for depth_image_name in depth_file_list:
+        file_path = os.path.join(depth_dir, depth_image_name)
+        depth_index = depth_image_name[0:COMMON_INDEX_BIT]
+        DEPTH_DICTIONARY[depth_index] = file_path
+    return
 
 
+# TODO Use priority queue to replace the map
 def sta_cal_single_image(clear, result, depth, psnr_map, group_id, divide):
     low_boundary = group_id * divide
     up_boundary = low_boundary + divide
@@ -81,27 +95,29 @@ def sta_read_image(clear_image_dir, result_image_dir, depth_map_dir):
 
 
 def sta_do_statistic(divide, thread_pool):
+    result_file_list = os.listdir(result_dir)
     # Read clear image, hazy image and their corresponding depth image.
     # Get the three corresponding matrices for a single image.
-    clear_single_dir = ""
-    result_single_dir = ""
-    depth_single_dir = ""
-    clear, result, depth = sta_read_image(clear_single_dir, result_single_dir, depth_single_dir)
+    for result_image_name in result_file_list:
+        result_single_dir = os.path.join(result_dir, result_image_name)
+        index = result_image_name[0:COMMON_INDEX_BIT]
+        clear_single_dir = CLEAR_DICTIONARY[index]
+        depth_single_dir = DEPTH_DICTIONARY[index]
+        clear, result, depth = sta_read_image(clear_single_dir, result_single_dir, depth_single_dir)
+        #  Traversal every pixel on the image and get a map for each group of the image.
+        #  image in the same group = old image .* map
+        # Calculate PSNR for all of the group respectively and record them into psnr_map.
+        task_list = []  # Create a list to save all tasks
+        psnr_map = {}  # Map to write PSNR result. Mutual information, need to add a lock.
+        for i in range(GROUP_NUM):
+            lst_vars = [clear, result, depth, psnr_map, i, divide]
+            func_var = [(lst_vars, None)]
+            task_list.append(threadpool.makeRequests(sta_cal_single_image, func_var))
+        for requests in task_list:
+            [thread_pool.putRequest(req) for req in requests]
+        thread_pool.poll()
 
-    #  Traversal every pixel on the image and get a map for each group of the image.
-    #  image in the same group = old image .* map
-    # Calculate PSNR for all of the group respectively and record them into psnr_map.
-    task_list = []  # Create a list to save all tasks
-    psnr_map = {}  # Map to write PSNR result. Mutual information, need to add a lock.
-    for i in range(GROUP_NUM):
-        lst_vars = [clear, result, depth, psnr_map, i, divide]
-        func_var = [(lst_vars, None)]
-        task_list.append(threadpool.makeRequests(sta_cal_single_image, func_var))
-    for requests in task_list:
-        [thread_pool.putRequest(req) for req in requests]
-    thread_pool.poll()
-
-    # Write the result into specific file.
+        # Write the result into specific file.
 
 
 def main():
@@ -110,7 +126,7 @@ def main():
     # Create a thread pool, # of thread = GROUP_NUM * 2.
     pool = threadpool.ThreadPool(GROUP_NUM * 2)
     # call dehazenet_input to read the images directory.
-    clear_image_list, hazy_image_list, depth_image_list = sta_image_input(clear_dir, hazy_dir, depth_dir)
+    sta_image_input(clear_dir, depth_dir)
     #  Start doing statistic calculation
     sta_do_statistic(divide, pool)
     pass
