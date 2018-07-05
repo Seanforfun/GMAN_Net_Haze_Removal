80462f1cbe944895e892436f51a4905ed9b466d9
Seanforfun <32276204+Seanforfun@users.noreply.github.com>
2018-07-05 12:01:05 -0400

    [Function add]
    1. Update the style of the setup.sh script.

--- DehazeNet/setup.sh
+++ DehazeNet/setup.sh
@@ -18,35 +18,35 @@ else
 	rm -rf $PROGRAM_ROOT_PATH
 fi
 cd $PROGRAM_ROOT_PATH
-echo $PROGRAM_ROOT_PATH is root directory for current project.
+echo [$PROGRAM_ROOT_PATH]: Root directory for current project.
 mkdir ClearImages
-echo $PROGRAM_ROOT_PATH/ClearImages is used to save clear images.
+echo [$PROGRAM_ROOT_PATH/ClearImages]: Save clear images.
 cd ./ClearImages
 mkdir TestImages
-echo $PROGRAM_ROOT_PATH/TestImages is used to save clear test images.
+echo [$PROGRAM_ROOT_PATH/TestImages]: Save clear test images.
 mkdir TrainImages
-echo $PROGRAM_ROOT_PATH/TrainImages is used to save clear training images.
+echo [$PROGRAM_ROOT_PATH/TrainImages]: Save clear training images.
 mkdir TransImages
-echo $PROGRAM_ROOT_PATH/TransImages is used to save transmission maps.
+echo [$PROGRAM_ROOT_PATH/TransImages]: Save transmission maps.
 cd ..
 mkdir ClearResultImages
-echo $PROGRAM_ROOT_PATH/ClearResultImages is used to save result images.
+echo [$PROGRAM_ROOT_PATH/ClearResultImages]: Save result images.
 mkdir HazeImages
-echo $PROGRAM_ROOT_PATH/HazeImages is used to save haze images.
+echo [$PROGRAM_ROOT_PATH/HazeImages]: Save haze images.
 cd ./ClearImages
 mkdir TestImages
-echo $PROGRAM_ROOT_PATH/TestImages is used to save haze test images.
+echo [$PROGRAM_ROOT_PATH/TestImages]: Save haze test images.
 mkdir TrainImages
-echo $PROGRAM_ROOT_PATH/TestImages is used to save haze training images.
+echo [$PROGRAM_ROOT_PATH/TestImages]: Save haze training images.
 cd ..
 mkdir StatisticalFigure
-echo $PROGRAM_ROOT_PATH/StatisticalFigure is used to save haze training images.
+echo [$PROGRAM_ROOT_PATH/StatisticalFigure]: Save haze training images.
 mkdir TFRecord
-echo $PROGRAM_ROOT_PATH/TFRecord is used to save tfrecords for training.
+echo [$PROGRAM_ROOT_PATH/TFRecord]: Save tfrecords for training.
 mkdir StatisticalResults
-echo $PROGRAM_ROOT_PATH/StatisticalResults is used to save Statistical Result for evaluating results.
+echo [$PROGRAM_ROOT_PATH/StatisticalResults]: Save Statistical Result for evaluating results.
 mkdir DeHazeNetModel
-echo $PROGRAM_ROOT_PATH/DeHazeNetModel is used to save training model.
+echo [$PROGRAM_ROOT_PATH/DeHazeNetModel]: Save training model.
 echo ------------------------------------------------------------------------------
 echo STEP 3: Copying files to your desired root path.
 echo ------------------------------------------------------------------------------
